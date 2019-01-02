7c0a6c2ea548b67e377303e7d4ade5d50edf480e
Hector Lucero <hectorqlucero@gmail.com>
2019-01-01 20:15:57 -0800

    Update and cleanup of libraries

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [clojure-csv/clojure-csv "2.0.2"]
-                 [compojure "1.6.1"]
+                 [compojure "1.6.1" :exclusions [commons-codec]]
                  [lib-noir "0.9.9"]
                  [com.draines/postal "2.0.3"]
                  [cheshire "5.8.1"]
                  [clj-pdf "2.3.0" :exclusions [commons-codec]]
-                 [pdfkit-clj "0.1.7"]
+                 [pdfkit-clj "0.1.7" :exclusions [commons-codec commons-logging]]
                  [clj-time "0.15.1"]
                  [date-clj "1.0.1"]
                  [clojurewerkz/money "1.10.0"]
@@ -19,8 +19,8 @@
                  [mysql/mysql-connector-java "8.0.13"]
                  [selmer "1.12.5" :exclusions [commons-codec]]
                  [inflections "0.13.0" :exclusions [commons-codec]]
-                 [ring/ring-devel "1.7.1"]
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring-devel "1.7.1" :exclusions [commons-codec ring/ring-codec]]
+                 [ring/ring-core "1.7.1" :exclusions [commons-codec ring/ring-codec]]
                  [ring/ring-anti-forgery "1.3.0"]
                  [ring/ring-defaults "0.3.2"]]
   :template-additions [".gitignore" "README.md"]
@@ -28,8 +28,7 @@
   :main ^:ccip-aot cc.core
   :aot [cc.core]
   :uberjar-name "cc.jar"
-  :plugins [[lein-ancient "0.6.10"]
-            [lein-pprint "1.1.2"]]
+  :plugins [[lein-pprint "1.2.0"]]
   :ring {:handler cc.core/app
          :auto-reload? true
          :auto-refresh? false}
