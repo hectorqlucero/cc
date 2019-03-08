f3e0a2f3edb933126c669c64c55d6c54c189928c
Hector Lucero <hectorqlucero@gmail.com>
2019-03-08 00:47:30 -0800

    Added carreras reporte process

--- src/cc/core.clj
+++ src/cc/core.clj
@@ -59,6 +59,7 @@
                           <a class='dropdown-item' href='/cartas/categorias'>Categorias</a>
                           <a class='dropdown-item' href='/admin/carreras'>Carreras</a>
                           <a class='dropdown-item' href='/cartas/exoneracion'>Registro Corredores</a>
+                          <a class='dropdown-item' href='/cartas/creporte'>Reporte Carrera</a>
                           <a class='dropdown-item' href='/cartas/puntos'>Actualizar Puntos</a>
                           <a class='dropdown-item' href='/cartas/ptotal'>Puntuación Total</a>
                           <a class='dropdown-item' href='/cron/crear/ciclistas'>Crear Ciclistas/Puntos</a>
