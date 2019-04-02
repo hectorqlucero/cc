6fc9d902c4efc8e75d84b6bf994339cb28f1d945
rexim <reximkut@gmail.com>
2019-04-02 11:08:28 -0700

    Cleaned namespace imports

--- src/cc/routes/talleres.clj
+++ src/cc/routes/talleres.clj
@@ -1,11 +1,6 @@
 (ns cc.routes.talleres
   (:require [cc.models.crud :refer :all]
-            [cc.models.grid :refer :all]
-            [cc.models.util :refer [fix-id user-level parse-int]]
-            [cheshire.core :refer :all]
             [compojure.core :refer :all]
-            [clj-pdf.core :refer :all]
-            [ring.util.io :refer :all]
             [selmer.parser :refer [render-file]]))
 
 ;; Start talleres
