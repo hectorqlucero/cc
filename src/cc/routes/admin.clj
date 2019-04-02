c581ce269f7b39358f946522386054e7c850cb91
rexim <reximkut@gmail.com>
2019-04-02 11:03:54 -0700

    Cleaned namespace imports

--- src/cc/routes/admin.clj
+++ src/cc/routes/admin.clj
@@ -1,8 +1,8 @@
 (ns cc.routes.admin
-  (:require [cc.routes.admin.cuadrantes :refer [cuadrantes-routes]]
-            [cc.routes.admin.users :refer [users-routes]]
-            [cc.routes.admin.carreras :refer [carreras-routes]]
+  (:require [cc.routes.admin.carreras :refer [carreras-routes]]
+            [cc.routes.admin.cuadrantes :refer [cuadrantes-routes]]
             [cc.routes.admin.taller :refer [taller-routes]]
+            [cc.routes.admin.users :refer [users-routes]]
             [compojure.core :refer :all]))
 
 (defroutes admin-routes
